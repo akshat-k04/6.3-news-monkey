fcf831cec65b557ad8aa8ca23277a3372d448a95
Akshat <a_khandelwal@ch.iitr.ac.in>
2022-12-14 10:18:37 +0530

    basic structure is made

---
+++ src/components/NewsItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  render() {
+    return (
+      <div>NewsItemhbh</div>
+    )
+  }
+}
+
+export default NewsItem
